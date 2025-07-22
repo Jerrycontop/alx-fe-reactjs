085c474dde2c188411cebfd08a5f23606dec965d
Nwalozie Jerry <jerrycontop@gmail.com>
2025-07-22 18:53:17 +0400

    Add my-company React website project

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer style={{ padding: "1rem", marginTop: "2rem", background: "#eee" }}>
+      <p>© 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
