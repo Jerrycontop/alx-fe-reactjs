f87bce5c1d48c979223a24980b51631f4af780a9
Jerry <jerrycontop@gmail.com>
2025-07-22 18:42:32 +0400

    Delete alx-react-app-props directory

--- alx-react-app-props/src/UserContext.js
+++
@@ -1,7 +0,0 @@
-// UserContext.js
-import React from 'react';
-
-// Initialize and export the UserContext
-const UserContext = React.createContext(null); // You can set a default value, but null is fine here
-
-export default UserContext;
--- alx-react-app-props/src/components/UserInfo.jsx
+++
@@ -1,9 +0,0 @@
-// UserInfo.jsx
-import UserDetails from './UserDetails';
-
-// Remove userData from props
-function UserInfo() {
-  return <UserDetails />; // No longer needs to pass userData as a prop
-}
-
-export default UserInfo;
