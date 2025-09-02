b8baedf46183d12dd701ae5796a74c2d7aceb888
Nwalozie Jerry <jerrycontop@gmail.com>
2025-09-02 22:04:44 +0400

    Added App.jsx and TodoList component

--- react-todo/src/components/TodoList.jsx
+++ react-todo/src/components/TodoList.jsx
@@ -1,41 +1,15 @@
-import React, { useState } from "react";
-import TodoItem from "./TodoItem";
-import AddTodo from "./AddTodo";
+import React from "react";
 
-function TodoList() {
-  const [todos, setTodos] = useState([]);
-
-  const addTodo = (text) => {
-    setTodos([...todos, { id: Date.now(), text, completed: false }]);
-  };
-
-  const toggleTodo = (id) => {
-    setTodos(
-      todos.map(todo =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
-  };
-
-  const deleteTodo = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id));
-  };
-
+const TodoList = () => {
   return (
     <div>
-      <AddTodo onAdd={addTodo} />
+      <h2>My Todo List</h2>
       <ul>
-        {todos.map(todo => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            onToggle={toggleTodo}
-            onDelete={deleteTodo}
-          />
-        ))}
+        <li>Learn React</li>
+        <li>Build Todo App</li>
       </ul>
     </div>
   );
-}
+};
 
 export default TodoList;
